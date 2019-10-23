f19764c07c024661fc8488c9a608448fdebbf301
Mikhail Volkov <mishanya3313@gmail.com>
2019-10-23 15:50:48 +0300

    added video

--- Video.js
+++ Video.js
@@ -6,13 +6,15 @@ export default class Video extends React.Component {
     constructor(props) {
         super(props);
         this.video = [
-            {title: "video", url:"http"},
-            {title: "video", url:"http"},
-            {title: "video", url:"http"},
-            {title: "video", url:"http"},
-            {title: "video", url:"http"},
-            {title: "video", url:"http"},
-            {title: "video", url:"http"},
+            {title: "Forest", url:"https://www.pexels.com/uk-ua/video/2081238/"},
+            {title: "Space", url:"https://www.pexels.com/uk-ua/video/857134/"},
+            {title: "City", url:"https://www.pexels.com/uk-ua/video/857266/"},
+            {title: "Woman", url:"https://www.pexels.com/uk-ua/video/2360466/"},
+            {title: "Flowers", url:"https://www.pexels.com/uk-ua/video/857189/"},
+            {title: "Lemons", url:"https://www.pexels.com/uk-ua/video/2583492/"},
+            {title: "Penguin", url:"https://www.pexels.com/uk-ua/video/1674470/"},
+            {title: "Water", url:"https://www.pexels.com/uk-ua/video/2360537/"},
+            {title: "Sea", url:"https://www.pexels.com/uk-ua/video/1093667/"},
         ]
     }
 
